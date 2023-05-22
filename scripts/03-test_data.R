4d48eacf0bb4e7f6f0e7f1a4f537fff1a8cb6371
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-05-22 20:17:46 +0000

    Updated personal information, date, and other information relevant to my data set.

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
 # Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Inessa De Angelis
+# Date: 22 May 2023
+# Contact: inessa.deangelis@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: none
 
 
 #### Workspace setup ####
