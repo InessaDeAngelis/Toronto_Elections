1e1f7b1b9339a33b6b07c41d92d7ad534ea0cc30
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-05-23 18:56:15 +0000

    Wrote data expectations, began writing simulations for the data with fictional wards, locations, and turnouts. Tests run so far have not been successful but further code needs to be added before a full simulation can be attampted.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -12,10 +12,36 @@
 library(tidyverse)
 
 #### Data expectations ####
+# There are a similar or identical number of polling locations per ward
+# The polling locations in each ward in equally spaced out within the ward boundaries
+# columns: ward, building_name, building_address, %_voted
 
 #### Simulate data ####
 
 ## Assumptions
 
+# Fictional ward, voting locations, and voter turnout
+sim_ward = c("Ward1", "Ward2", "Ward3", "Ward44")
+sim_locations = c(55, 52, 57, 56)
+sim_turnout = c("22%", "27%", "32%", "38%")
 
+## Create summary statistics of number of polling locations per ward and 
+## compare it with the voter turnout by ward
 
+# summarize polling locations by ward
+polling_locations_per_ward = 
+  simulated_data |>
+  group_by(ward) |>
+  count() |>
+  rename("num_locations" = "n")
+
+# add voter turnout column to the data
+locations_per_ward['turnout'] = sim_turnout
+
+# compute bps per ward
+locations_per_ward =
+  locations_per_ward |>
+  mutate(
+    bps_of_ward =
+      num_locations / population * 10000
+  )
