fafd86e295987249593a41c72d4ccf57af2b243c
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-05-25 20:41:49 +0000

    updated relevant information and removed example code.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -5,15 +5,17 @@
 # Contact: inessa.deangelis@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: none
-# Data sets: https://open.toronto.ca/dataset/elections-voting-locations/ 
-# Note: Only download the 2022 election data from the webpage
+# Data sets:
+## - https://open.toronto.ca/dataset/elections-voting-locations/ 
+## - https://open.toronto.ca/dataset/elections-voter-statistics/ 
+# Note: Only download the 2022 election data from the webpages
 
 #### Workspace setup ####
+# install.packages("tidyverse")
 library(tidyverse)
 
 #### Data expectations ####
 # There are a similar or identical number of polling locations per ward
-# The polling locations in each ward in equally spaced out within the ward boundaries
 # columns: _id, point_name, point_short_code, voter_count
 
 #### Simulate data ####
@@ -22,28 +24,14 @@ library(tidyverse)
 
 # Fictional point name, point short code, and voter count
 sim_id = c("1", "2", "3", "4")
-sim_point_name = c("Catholic_Church", "High_School", "Apartments", "Nursing_Home")
+sim_point_name = c("Church", "High_School", "Apartments", "Nursing_Home")
 sim_point_short_code = c(042, 025, 033, 060)
 sim_voter_count = c("1000", "1700", "500", "2100")
 
-## Create summary statistics of number of polling locations per ward and 
-## compare it with the voter turnout by ward
+## Create summary statistics of number of polling locations per ward 
 
 # summarize polling locations by ward
-polling_locations_per_ward = 
+locations_per_ward = 
   simulated_data |>
   group_by(ward) |>
   count() |>
-  rename("num_locations" = "n")
-
-# add point name column to the data
-locations_per_ward['turnout'] = sim_point_turnout
-
-# compute bps per ward
-locations_per_ward =
-  locations_per_ward |>
-  mutate(
-    bps_of_ward =
-      num_locations / population * 10000
-  )
-
