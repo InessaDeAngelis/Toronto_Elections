9a9f860d0aad363827dd324f99c22a4703e7cfa1
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-05-25 17:48:56 +0000

    re-added previously removed code for voter statistics

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -6,7 +6,6 @@
 # License: MIT
 # Pre-requisites: none
 
-
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
@@ -18,12 +17,25 @@ source("scripts/04-helper_functions.R")
 
 # download data
 raw_voting_locations = download_data_from_opendatatoronto (
-package_id = "31dac8b2-2e15-4945-abef-ce98d248bb8e",
-resource_id = "31dac8b2-2e15-4945-abef-ce98d248bb8e"
+  package_id = "31dac8b2-2e15-4945-abef-ce98d248bb8e",
+  resource_id = "31dac8b2-2e15-4945-abef-ce98d248bb8e"
 )
 
 # write data
 write.csv (
   x = raw_voting_locations,
   file = "inputs/data/raw_voting_locations.csv"
+)
+
+#### Download data and write municipal election voter statistics data ####
+
+# download data
+raw_voter_statistics = download_data_from_opendatatoronto (
+  package_id = "7dc606ab-f042-4d90-99e2-9247cb5953d2",
+)
+
+# write data
+write.csv (
+  x = raw_voter_statistics,
+  file = "inputs/data/raw_voter_statistics.csv"
 )
