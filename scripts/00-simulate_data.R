e812ba13d3163c32a0913a2556c4768a3bea9243
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-05-27 15:49:36 +0000

    adding code, using the previous codes in the script, to cimpare the voter turnout per ward with the number of subdivisions per ward.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -51,3 +51,21 @@ simulate_data <-
 
 simulate_data
 
+#### Simulate comparing the voter turnout to number of subdivisions per ward ####
+
+set.seed(300)
+
+simulate_data <-
+  tibble(
+    #use 1 through 25 to represent each ward
+    "ward" = 1:25,
+    #randomly pick an option, with replacement, 25 times
+    "turnout" = sample(
+      x = c("25%", "22%", "30%", "34%", "38%"),
+      size = 25,
+      replace = TRUE,
+      #randomly pick an option, between 50 and 75, without replacement, 25 times
+      "number_subdivision" = x <- sample(50:75, 25, replace=F),
+    )
+  )
+simulate_data
