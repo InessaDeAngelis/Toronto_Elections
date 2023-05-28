a97121bb911d867e86f06fd4186266ef0f85cff6
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-05-28 23:31:13 +0000

    Updated script organization with re-naming of headers and sub-headers. Removed example code. Began writing code to select specific columns of interest from the voter statistics spreadsheet. Will return to this section of code to resolve errors.

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -10,39 +10,21 @@
 library(tidyverse)
 library(janitor)
 
-#### Clean data ####
+#### Read in raw data ####
+
 # Read in the raw voter statistics data. 
 readr::read_csv("inputs/data/raw_voter_statistics.csv")
 
 # Read in the raw voting locations data. 
 readr::read_csv("inputs/data/raw_voting_locations.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+#### Data Cleaning ####
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+# select columns of interest #
+raw_voter_statistics |>
+  select(
+    ward,
+    sub,
+    total_eligible_electors,
+    number_voted,
+  )
